095792059a6dd3a56732ca69786a3d02ac746c77
Galyna Maikovych <gmaik.com@gmail.com>
2025-04-05 18:12:43 +0300

    make build

---
+++ js_center_spider_DOM.8142fd2e.js
@@ -0,0 +1,2 @@
+"use strict";function center(){var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientWidth/2-e.clientWidth/2,n=t.clientHeight/2-e.clientHeight/2;e.style.left="".concat(c,"px"),e.style.top="".concat(n,"px")}center();
+//# sourceMappingURL=js_center_spider_DOM.8142fd2e.js.map
